df6e21350fcc78cfd12279bb009a81e90bdccf7a
Slok7565 <slok7565@qq.com>
2023-11-09 17:59:55 +0800

    Update Announcement

--- Patches/AnnouncementPatch.cs
+++ Patches/AnnouncementPatch.cs
@@ -68,6 +68,54 @@ public class ModNewsHistorySCn
         *别忘了标点！！！！！
         */
         {
+
+            var news = new ModNewsSCn
+            {
+                Number = 100008,
+                Title = "TownOfHostEdited-Xi v2.0.5",
+                SubTitle = "★★★★Bug修复★★★★",
+                ShortTitle = "★TOHEX v2.0.5★\n<size=75%>简体中文</size>",
+                Text =
+                "简体中文"
+                + "\n-----------------------------"
+                + "\n<size=125%>欢迎来到TOHEX,感谢您的游玩</size>"
+                + "\n-----------------------------"
+                + "\n终于解决了!"
+                + "\n\n\n### 对应官方版本"
+                + "\n- 基于TOH版本 v4.1.2"
+                + "\n- 基于TOHE版本 v2.3.55"
+                + "\n- 适配Among Us版本 v2023.10.24及以上版本"
+                + "\n\n### 修复"
+                + "\n- 修复了击杀检查失效的问题"
+                + "\n- 修复了原版职业工程师为模型的职业不可使用通风口的问题"
+                + "\n- 修复了迷你船员长大不显示的问题",
+                Date = "2023-11-09T00:00:00Z",
+            };
+            AllModNews.Add(news);
+        }
+        {
+            var news = new ModNewsSCn
+            {
+                Number = 100007,
+                Title = "TownOfHostEdited-Xi v2.0.5",
+                SubTitle = "★★★★Bugs Fix★★★★",
+                ShortTitle = "★TOHEX v2.0.5★\n<size=75%>English</size>",
+                Text =
+                "English"
+                + "\n-----------------------------"
+                 + "\n<size=125%>Welcome To TOHEX,Thank ya For Playing!</size>"
+                + "\n-----------------------------"
+                + "\nOOOOOOOKKKKKAAAAYYYYY!"
+               + "\n\n\n### Support Among Us Version"                + "\n- Based On TOH v4.1.2"                + "\n- Based On TOHE v2.3.55"                + "\n- Support Among Us v2023.10.24 And Above"                + "\n\n### Bugs Fix"
+                + "\n- Check Murder Lose Efficacy Bug Fix"
+                + "\n- Engineer Modle's Role Can't Vent Bug Fix"
+                + "\n- When Mini Grows Up Didn't Displayed In Mod Client Bug Fix",
+                 
+                Date = "2023-11-09T00:00:00Z",
+            };
+            AllModNews.Add(news);
+        }
+        {
             var news = new ModNewsSCn
             {
                 Number = 100006,
